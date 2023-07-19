0775ef8885780c8803dd9314d03d1c7ce9a4dd98
asyncguo <asyncguo@163.com>
2023-07-19 21:46:30 +0800

    feat: ResponsiveGrid 重构

--- src/responsive-design/components/ResponsiveGrid/index.tsx
+++ src/responsive-design/components/ResponsiveGrid/index.tsx
@@ -1,109 +1,124 @@
-import { useDevice, DeviceName } from "@/responsive-design";
 import React from "react";
+import { useDevice } from "@/responsive-design";
 import { Col, ColProps, Row, RowProps } from 'antd'
 
 /** 栅格系统的 column 个数 */
 const LAYOUT_COLUMNS = 24
 
-interface IResponsiveGridProps extends RowProps {
-  /** children 组件名称 */
-  childrenName?: 'Grid' | 'Descriptions' | 'ProDescriptions'
+/**
+ * 处理 antd 的 Row 
+ * @link https://4x.ant.design/components/grid-cn/
+ * @example 
+ * <ResponsiveGrid.Grid>
+ *  <Row>
+ *    <Col></Col>
+ *    <Col></Col>
+ *  </Row>
+ * </ResponsiveGrid.Grid>
+ */
+const ResponsiveGridEnhancer = (props: React.PropsWithChildren) => {
+  const { children } = props
+  const { layoutColSpan } = useDevice()
+
+  if (React.isValidElement(children)) {
+    const { children: colChildren } = children.props
+    console.log('colChildren',colChildren);
+    
+    const array = React.Children.toArray(colChildren)
+    
+    return React.cloneElement(
+      children,
+      {
+        children: array.map((child) => {
+          if (React.isValidElement(child)) {
+            return React.cloneElement(
+              child,
+              {
+                // 若外部 Col 设置了 xs、sm、md、lg、xl、xxl，span 不再生效（与原始 span 效果一致）
+                span: layoutColSpan,
+              } as ColProps
+            )
+          }
+  
+          return child
+        })
+      }
+    )
+  }
+
+  return children
 }
 
 /**
- * Grid enhancer
- * @link https://4x.ant.design/components/grid-cn/
+ * 处理 antd 的 Descriptions 
+ * @link https://4x.ant.design/components/descriptions-cn/
+ * <ResponsiveGrid.Descriptions>
+ *  <Descriptions>
+ *    <Descriptions.Item></Descriptions.Item>
+ *    <Descriptions.Item></Descriptions.Item>
+ *  </Descriptions>
+ * </ResponsiveGrid.Descriptions>
  */
-const gridEnhancer = (children: React.ReactElement, options: {
-  layoutColSpan: number
-}) => {
-  const { layoutColSpan } = options
-  const { children: colChildren } = children.props
-  const array = React.Children.toArray(colChildren)
-  
-  return React.cloneElement(
-    children as React.ReactElement,
-    {
-      children: array.map((child) => {
-        if (React.isValidElement(child)) {
-          return React.cloneElement(
-            child,
-            {
-              // 若外部 Col 设置了 xs、sm、md、lg、xl、xxl，span 不再生效（与原始 span 效果一致）
-              span: layoutColSpan,
-            } as ColProps
-          )
-        }
+const ResponsiveDescriptions = (props: React.PropsWithChildren) => {
+  const { children } = props
+  const { name: deviceName, layoutColSpan } = useDevice()
 
-        return child
+  if (React.isValidElement(children)) {
+    const descriptionsProps = {
+      column: LAYOUT_COLUMNS / layoutColSpan,
+    }
+  
+    if (deviceName === 'phone') {
+      // 在移动端上，描述项的样式保持【左右对齐】，与店员端样式拉齐
+      Object.assign(descriptionsProps, {
+        contentStyle: {
+          ...(children.props?.contentStyle || {}),
+          justifyContent: 'flex-end'
+        }
       })
     }
-  )
+    return React.cloneElement(
+      children,
+      descriptionsProps
+    )
+  }
+
+  return children
 }
 
 /**
- * Descriptions enhancer
- * @link https://4x.ant.design/components/descriptions-cn/
- */
-const descriptionsEnhancer = (children: React.ReactElement, options: {
-  layoutColSpan: number
-  deviceName: DeviceName
-}) => {
-  const { deviceName, layoutColSpan } = options
-  const descriptionsProps = {
-    column: LAYOUT_COLUMNS / layoutColSpan,
-  }
-  if (deviceName === 'phone') {
-    // 在移动端上，描述项的样式保持【左右对齐】，与店员端样式拉齐
-    Object.assign(descriptionsProps, {
-      contentStyle: {
-        ...(children.props?.contentStyle || {}),
-        justifyContent: 'flex-end'
-      }
-    })
-  }
-  return React.cloneElement(
-    children,
-    descriptionsProps
-  )
-}
-
-/**
- * ProDescriptions enhancer
+ * 处理 ProDescriptions
  * @link https://procomponents.ant.design/components/descriptions
+ * <ResponsiveGrid.ProDescriptions>
+ *  <ProDescriptions>
+ *    <ProDescriptions.Item></ProDescriptions.Item>
+ *    <ProDescriptions.Item></ProDescriptions.Item>
+ *  </ProDescriptions>
+ * </ResponsiveGrid.ProDescriptions>
  */
-const proDescriptionsEnhancer = (children: React.ReactElement, options: {
-  layoutColSpan: number
-  deviceName: DeviceName
-}) => {
-  const { deviceName, layoutColSpan } = options
-  
-  return descriptionsEnhancer(children, {
-    layoutColSpan,
-    deviceName
-  })
+const ResponsiveProDescriptions = (props: React.PropsWithChildren) => {
+  return <ResponsiveDescriptions {...props} />
 }
 
 /**
  * 响应式栅格布局
  */
-const ResponsiveGrid = (props: React.PropsWithChildren<IResponsiveGridProps>) => {
-  const { 
-    childrenName,
+const ResponsiveGrid = (props: React.PropsWithChildren<RowProps>) => {
+  const {
     children,
     ...rowProps
   } = props
-  const { name: deviceName, layoutColSpan } = useDevice()
-  
-  if (Array.isArray(children)) {
+  const { layoutColSpan } = useDevice()
+
+  if (React.isValidElement(children) || Array.isArray(children)) {
     const array = React.Children.toArray(children)
-    console.log('array',array);
     
     return (
       <Row {...rowProps}>
         {
           array.map((child, index) => {
             if (React.isValidElement((child))) {
+              // TODO: child.type type is ?
               const displayName = (child.type as any).displayName
               
               if (displayName === 'ResponsiveGrid_Col') {
@@ -130,30 +145,13 @@ const ResponsiveGrid = (props: React.PropsWithChildren<IResponsiveGridProps>) =>
       </Row>
     )
   }
-
-  if (!React.isValidElement(children)) return children
-
-  const originalChildren = children as React.ReactElement
-  // 以下针对现有组件（仅对 Grid、Descriptions、）做 hack 处理
-  switch (childrenName) {
-    case 'Grid':
-      return gridEnhancer(originalChildren, {
-        layoutColSpan
-      })
-    case 'Descriptions':
-      return descriptionsEnhancer(originalChildren, {
-        deviceName,
-        layoutColSpan
-      })
-    case 'ProDescriptions':
-      return proDescriptionsEnhancer(originalChildren, {
-        deviceName,
-        layoutColSpan
-      })
-  }
   
   return children
 };
+
+ResponsiveGrid.Grid = ResponsiveGridEnhancer
+ResponsiveGrid.Descriptions = ResponsiveDescriptions
+ResponsiveGrid.ProDescriptions = ResponsiveProDescriptions
 
 ResponsiveGrid.Col = Col
 ResponsiveGrid.Col.displayName = 'ResponsiveGrid_Col'
